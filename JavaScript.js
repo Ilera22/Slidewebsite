195b99f3e95ee779ac0978a8cfbd473c6142d8bf
Ilera22 <108095478+Ilera22@users.noreply.github.com>
2024-05-14 13:18:57 -0500

    C

--- JavaScript.js
+++ JavaScript.js
@@ -1,10 +1,28 @@
 document.querySelectorAll('.navigation a').forEach(anchor => {
   anchor.addEventListener('click', function(e) {
-    e.preventDefault();
-    const target = document.querySelector(this.getAttribute('href'));
-    window.scrollTo({
-      top: target.offsetTop - 50,
+    e.preventDefault(); // Prevenir el comportamiento por defecto de los enlaces
+    const targetId = this.getAttribute('href');
+    const targetSection = document.querySelector(targetId);
+    
+    targetSection.scrollIntoView({
       behavior: 'smooth'
     });
   });
+});
+
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('is-visible');
+    } else {
+      entry.target.classList.remove('is-visible');
+    }
+  });
+}, {
+  threshold: 0.5 // 50% de la sección debe estar visible
+});
+
+document.querySelectorAll('.section').forEach(section => {
+  observer.observe(section);
 });
