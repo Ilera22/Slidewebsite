898bb9115f281280596939fbfafdf6747a6107a6
Ilera22 <108095478+Ilera22@users.noreply.github.com>
2024-05-14 16:59:04 -0500

    Otro cambio

--- JavaScript.js
+++ JavaScript.js
@@ -1,33 +1,23 @@
-document.querySelectorAll('.navigation a').forEach(anchor => {
-  anchor.addEventListener('click', function(e) {
-    e.preventDefault(); // Prevenir el comportamiento por defecto de los enlaces
-    const targetId = this.getAttribute('href');
-    const targetSection = document.querySelector(targetId);
-    
-    targetSection.scrollIntoView({
-      behavior: 'smooth'
-    });
-  });
-});
-
-
 document.addEventListener("DOMContentLoaded", function() {
-  // Crear un observador con IntersectionObserver
-  const observer = new IntersectionObserver((entries, observer) => {
+  const sections = document.querySelectorAll('.section');
+
+  const observer = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
-      if (entry.isIntersecting) { // Verificar si la sección está visible
-        entry.target.classList.add('is-visible'); // Añadir clase para animación
-        observer.unobserve(entry.target); // Opcional: Dejar de observar después de animar
+      if (entry.isIntersecting) {
+        let previousSection = entry.target.previousElementSibling;
+        // Asegurar que la animación se aplique cuando la sección previa casi ha salido de la vista
+        if (previousSection && !previousSection.classList.contains('is-visible')) {
+          previousSection.classList.add('is-visible');
+        }
+        entry.target.classList.add('is-visible');
       }
     });
   }, {
-    rootMargin: '0px',
-    threshold: 0.5 // Ajusta esto para cambiar cuándo se activa la animación
+    rootMargin: "0px",
+    threshold: 0.05 // Un umbral más pequeño para empezar la animación más temprano
   });
 
-  // Aplicar el observador a cada sección
-  document.querySelectorAll('.section').forEach(section => {
+  sections.forEach(section => {
     observer.observe(section);
   });
 });
-
